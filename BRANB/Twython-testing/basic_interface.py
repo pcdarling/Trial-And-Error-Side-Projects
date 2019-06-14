5e4e9e32992b6b406e7d113869a6a7f1475a5fc6
Brandon McGrew <42500154+xxiFoxTrot@users.noreply.github.com>
2019-06-13 19:58:25 -0500

    implemented more main Twython functions

--- BRANB/Twython-testing/basic_interface.py
+++ BRANB/Twython-testing/basic_interface.py
@@ -1,31 +1,68 @@
 from twython import Twython
 
-APP_KEY = "P3TyZI4ELKYnKQf56O4hdFcez"  # brandon's key"VdxItbS6YKi6KibcsS07o1Onh"
-APP_SECRET = "JfRmg4VGCiK8sv7iTK0uF13bFExiKluWRJexKdi2J2MVZRG2kU"  # brandon's secret"ZmeXbVMiMaAwuxCmDMpXwZcLfKU6xz3BgZXZrBy9ZfETFxkDlz"
+APP_KEY = "VdxItbS6YKi6KibcsS07o1Onh"
+APP_SECRET = "ZmeXbVMiMaAwuxCmDMpXwZcLfKU6xz3BgZXZrBy9ZfETFxkDlz"
 
-OAUTH_TOKEN = "999809546636922880-0YTHtdtIwx4EDszsu4SsZpghyP3BIID"  # brandon's token"2749858793-UiAdvAuAEOS8aQVCHrWmeHL3lsfEIuhWXnVwdzA"
-OAUTH_TOKEN_SECRET = "ARHB1ofOUdgltXxRbyuzUwvMSXh8N0Je5YW8TIWMU4YPY"  # brandon's token secret"WJvX90jzLaRO9FduM1ozkP6jU8O5cW7VxtHJUv8hghTcU"
+OAUTH_TOKEN = "2749858793-UiAdvAuAEOS8aQVCHrWmeHL3lsfEIuhWXnVwdzA"
+OAUTH_TOKEN_SECRET = "WJvX90jzLaRO9FduM1ozkP6jU8O5cW7VxtHJUv8hghTcU"
 
-user_input = ""
+
+function_arr = ['"tweet" - allows you to post a tweet','"search user" - get the most recent posts from a user','"search twitter" - find posts based on keyword(s)', '"exit" - this will take you out of the program' ]
 
 t = Twython(APP_KEY,APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
 
 def main():
-
-	print('Welcome to Twython! For a list of commands, type "help()". To exit the program, type"exit()"')
-	user_input = input("Enter command here!")
+	print('Welcome to Twython! For a list of commands, type "help". To exit the program, type"exit"')
+	user_input = input("Enter command here!-->")
 	if (user_input == 'tweet'):
 		create_tweet()
+	elif(user_input == 'search user'):
+		fetch_timeline()
+	elif(user_input == 'help'):
+		help()
+	elif(user_input == 'exit'):
+		exit()
+	else:
+		print('command was not valid. For a list of commands, please type "help"')
+		main()
 
 def create_tweet():
-	user_input = ""
-	inside_tweet = True
-	print('what would you like to tweet?')
-	while(inside_tweet):
-		#waiting for further user input to determine what happens next
-		if(user_input != ""):
-			print("sending tweet...")
-			t.update_status(status=user_input)
-			print('tweet posted!')
-			user_input= ""
-			inside_tweet=False
+	tweet_content = input('what would you like to tweet?')
+	if(tweet_content == ""):
+		print('you have to type something in, silly')
+		tweet_content = input('what would you like to tweet?')
+	elif((tweet_content == 'cancel') or (tweet_content == 'back') or (tweet_content == 'exit')):
+		print('returning to main menu')
+		main()
+	else:
+		print("sending tweet...")
+		t.update_status(status=tweet_content)
+		print('tweet posted!')
+		inside_tweet=False
+		main()
+
+def fetch_timeline():
+	user = input('which user do you want to see?')
+	if(user == ""):
+		print('you have to type something in, silly')
+		user = input('which user do you want to see?')
+	elif((user == 'cancel') or (user == 'back') or (user == 'exit')):
+		print('returning to main menu')
+		main()
+	else:
+		user_timeline = t.get_user_timeline(screen_name=user, count=40, include_rts=False, exclude_replies=True)
+		for i in range(20):
+			timeline_results = user_timeline[i]
+			print(timeline_results['text']
+		main()
+
+
+def help():
+	print('These are the list of commands you can use in this interface:')
+	print(function_arr)
+	main()
+
+def exit():
+	print('Now leaving twython terminal interface. Goodbye world!')
+if(__name__ == "__main__"):
+	main()
